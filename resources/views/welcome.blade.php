d3b315a3bf41eda45b1f77c6f77045401e2408bb
Rey V. Aquino <49036511+includeiostreamsharp@users.noreply.github.com>
2021-02-27 21:23:24 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Developer
                 </div>
 
                 <div class="links">

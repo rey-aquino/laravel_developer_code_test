d90709c91e8e4d90fd58525859b2cd3b81ec11d5
Rey V. Aquino <49036511+includeiostreamsharp@users.noreply.github.com>
2021-02-27 21:28:55 +0800

    Add Suppliers

---
+++ app/Http/Controllers/User/AddSupplierController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Country;
+use DB;
+
+class AddSupplierController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $countries = Country::all();
+        return view('users.suppliers.add', compact('countries'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $request->validate([
+            'name'         => 'required',
+            'address'      => 'required',
+            'phone_number' => 'required',
+            'email'        => 'required',
+            'city'         => 'required',
+            'state'        => 'required',
+            'country'      => 'required',
+        ]);
+
+        DB::table('suppliers')->insert([
+            'name'         => $request->name,
+            'address'      => $request->address,
+            'phone_number' => $request->phone_number,
+            'email'        => $request->email,
+            'city'         => $request->city,
+            'state'        => $request->state,
+            'country'      => $request->country,
+        ]);
+        return back();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
